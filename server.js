a8e9e0404286c9c696eb517a58062dbe88ed999f
Brian Ford <bford2344@gmail.com>
2019-04-17 10:19:48 -0600

    passport time

--- server.js
+++ server.js
@@ -67,13 +67,31 @@ app.get('/signup', authController.signup);
  
     app.get('/signin', authController.signin);
 
-    app.get('/dashboard', authController.dashboard);
+    app.get('/dashboard', isLoggedIn, authController.dashboard);
 
+    app.get('/logout', authController.logout);
 
+    function isLoggedIn(req, res, next) {
+ 
+        if (req.isAuthenticated())
+         
+            return next();
+             
+        res.redirect('/signin');
+     
+    }
 app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/dashboard',
 
     failureRedirect: '/signup'
+}
+
+));
+
+app.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/dashboard',
+
+    failureRedirect: '/signin'
 }
 
 ));
